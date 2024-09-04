b7241ad3c333c7619c5c23daf63d564d2084336e
Joaokenehen <115033870+Joaokenehen@users.noreply.github.com>
2024-09-03 21:44:10 -0300

    Add files via upload

---
+++ MainEstudo/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static void main( String[] args){
+
+        Integer num1 = 1;
+        String palavr = "Oi";
+        Boolean sim = true;
+        Double num2 = 5.76;
+        Character b = '@';
+
+        System.out.println(num1);
+        System.out.println(palavr);
+        System.out.println(sim);
+        System.out.println(num2);
+        System.out.println(b);
+    }
+}
+
